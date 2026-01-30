de2711fd0f646a1ca0c56a14e21b8c7888303b45
Susanta Gorai <susantagorai@gmail.com>
2026-01-30 10:41:40 +0530

    Diamond Problem

---
+++ OOPS/diamondProblem.cpp
@@ -0,0 +1,29 @@
+// Diamond Problem in C++ using Virtual Inheritance
+#include<iostream>
+using namespace std;
+class A{
+    public: 
+    void Print(){
+        cout<<"Hello from A"<<endl;
+    }
+};
+class B: virtual public A{
+    
+};
+
+class C: virtual public A{
+    
+};
+
+class D: public B, public C{
+    
+};
+int main(){
+    D obj;
+    //obj.Print(); // Error: request for member 'Print' is ambiguous
+    // obj.B::Print(); // Resolving ambiguity by specifying the path
+    // obj.C::Print(); // Resolving ambiguity by specifying the path
+    obj.Print();
+
+    return 0;
+}
